186b0448d636ecf21f0462d437e4a23baf8c57ef
Demerro <79833946+Demerro@users.noreply.github.com>
2024-11-23 18:55:31 +0300

    Initial commit

---
+++ Mastodon-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mastodon-Clone
+//
+//  Created by Nikita Prokhorchuk on 23.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
