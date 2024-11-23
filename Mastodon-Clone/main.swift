afeda16af6d8abf3a3eedf26d4cd97e197371e89
Demerro <79833946+Demerro@users.noreply.github.com>
2024-11-23 19:06:53 +0300

    Project setup

---
+++ Mastodon-Clone/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Mastodon-Clone
+//
+//  Created by Nikita Prokhorchuk on 23.11.24.
+//
+
+import UIKit
+
+UIApplicationMain(
+  CommandLine.argc,
+  CommandLine.unsafeArgv,
+  nil,
+  NSStringFromClass(AppDelegate.self)
+)
